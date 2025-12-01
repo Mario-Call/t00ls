9b5f294bbe8f41546c5a7cbb1ac64de29f691929
Mario-Call <2813955543@qq.com>
2025-12-01 11:02:39 +0800

    更新ico

---
+++ .vitepress/theme/assets/linkData.mjs
@@ -0,0 +1,39 @@
+// 链接数据
+const linkData = [
+  // 推荐数据
+  {
+    type: "rec",
+    typeName: "推荐",
+    typeDesc: "安全相关",
+    typeList: [
+      {
+        name: "T00ls",
+        avatar: "https://t00ls.com/static/images/logo.png",
+        desc: "T00ls",
+        url: "https://t00ls.com/",
+      },
+      {
+        name: "马里奥'Blog",
+        avatar: "/images/logo/logo.webp",
+        desc: "分享技术与科技生活",
+        url: "https://www.t00ls.cc/",
+      },
+    ],
+  },
+  // 小伙伴们
+  {
+    type: "friends",
+    typeName: "小伙伴们",
+    typeDesc: "我们在一起，共同进步",
+    typeList: [
+      {
+        name: "马里奥'Blog",
+        avatar: "/images/logo/logo.webp",
+        desc: "分享技术与科技生活",
+        url: "https://www.t00ls.cc/",
+      },
+    ],
+  },
+];
+
+export default linkData;
